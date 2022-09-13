5ce25f3ddb295fcb727a99dd988fecf0b91e33ee
Blayne Bayer <bbayer1@swbell.net>
2022-09-13 15:44:50 -0500

    fix: fix add device

--- Sources/Passage/PassageAuth.swift
+++ Sources/Passage/PassageAuth.swift
@@ -799,13 +799,6 @@ public class PassageAuth {
         var authResult: AuthResult?
         do {
             let startResponse = try await PassageAPIClient.shared.addDeviceStart(token: token)
-//            guard let identifier = startResponse.handshake.challenge.publicKey.user.name else {
-//                throw PassageError.unknown
-//            }
-//            var identifier = startResponse.user.email
-//            if (identifier == nil) {
-//                identifier = startResponse.user.phone
-//            }
             let registrationRequest = try await RegistrationAuthorizationController.shared.register(from: startResponse, identifier: startResponse.handshake.challenge.publicKey.user.name )
             authResult = try await PassageAPIClient.shared.addDeviceFinish(token: token, startResponse: startResponse, params: registrationRequest!)
         }
