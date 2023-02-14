2389ce2e7eb6d6207fc5549b1bf6c2a4d1cfbc14
Blayne Bayer <blayne.bayer@passage.id>
2023-02-14 11:06:02 -0600

    basic first integration test

--- Tests/Integration/PassageIntegrationTestData.swift
+++ Tests/Integration/PassageIntegrationTestData.swift
@@ -5,4 +5,23 @@
 //  Created by blayne bayer on 2/14/23.
 //
 
-import Foundation
+import AuthenticationServices
+@testable import Passage
+
+let unregisteredUserEmail = "unregistered-test-user@passage.id"
+
+
+let appInfoTest = AppInfo(
+    id: "TEST_APP_ID",
+    ephemeral: true,
+    name: "TEST_APP",
+    redirect_url: "TEST_APP_URL",
+    login_url: "TEST_LOGIN_URL",
+    allowed_identifier: "TEST_ALLOWED_IDENTIFIER",
+    required_identifier: "TEST_REQUIRED_IDENTIFIER",
+    auth_origin: "TEST_AUTH_ORIGIN",
+    require_email_verification: false,
+    require_identifier_verification: false,
+    session_timeout_length: 6000,
+    public_signup: true
+)
--- Tests/Integration/PassageIntegrationTests.swift
+++ Tests/Integration/PassageIntegrationTests.swift
@@ -8,18 +8,18 @@
 import XCTest
 @testable import Passage
 
-final class testss: XCTestCase {
+final class PassageIntegrationTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        PassageSettings.shared.appId = testAppInfo.id
-        PassageSettings.shared.authOrigin = testAppInfo.auth_origin
-        PassageSettings.shared.apiUrl = "TEST_API_URL"
-        PassageAPIClient.shared = MockPassageAPIClient()
-        if #available(iOS 16.0, *) {
-            LoginAuthorizationController.shared = MockLoginAuthorizationController()
-            RegistrationAuthorizationController.shared = MockRegistrationAuthorizationController()
-        }
+        PassageSettings.shared.appId = appInfoTest.id
+        PassageSettings.shared.authOrigin = appInfoTest.auth_origin
+        PassageSettings.shared.apiUrl = "https://auth-uat.passage.dev"
+//        PassageAPIClient.shared = MockPassageAPIClient()
+//        if #available(iOS 16.0, *) {
+//            LoginAuthorizationController.shared = MockLoginAuthorizationController()
+//            RegistrationAuthorizationController.shared = MockRegistrationAuthorizationController()
+//        }
     }
     
     override func tearDown() {
@@ -36,48 +36,48 @@ final class testss: XCTestCase {
         XCTAssertEqual(thrownError as? PassageError, .userDoesNotExist)
     }
     
-    func testPassageAuth_whenRegisterExistingUser_throwsAlreadyExistsError() async {
-        var thrownError: Error?
-        do {
-            let _ = try await PassageAuth.register(identifier: registeredUserEmail)
-        } catch {
-            thrownError = error
-        }
-        XCTAssertEqual(thrownError as? PassageError, .userAlreadyExists)
-    }
-    
-    func testPassageAuth_whenLoginExistingUser_returnsValidAuthResult() async {
-        let result = try? await PassageAuth.login(identifier: registeredUserEmail)
-        if #available(iOS 16.0, *) {
-            XCTAssertTrue(result?.authResult is AuthResult)
-        } else {
-            XCTAssertTrue(result?.magicLink is MagicLink)
-        }
-    }
-    
-    func testPassageAuth_whenRegisterNewUser_returnsValidAuthResult() async {
-        let result = try? await PassageAuth.register(identifier: unregisteredUserEmail)
-        if #available(iOS 16.0, *) {
-            XCTAssertTrue(result?.authResult is AuthResult)
-        } else {
-            XCTAssertTrue(result?.magicLink is MagicLink)
-        }
-    }
-    
-    func testPassageAuth_whenGetExistingUser_returnsUser() async {
-        let user = try? await PassageAuth.getUser(identifier: registeredUserEmail)
-        XCTAssertNotNil(user)
-    }
-    
-    func testPassageAuth_whenGetNonexistingUser_throwsError() async {
-        var thrownError: Error?
-        do {
-            let _ = try await PassageAuth.getUser(identifier: unregisteredUserEmail)
-        } catch {
-            thrownError = error
-        }
-        XCTAssertEqual(thrownError as? PassageError, .userDoesNotExist)
-    }
-    
+//    func testPassageAuth_whenRegisterExistingUser_throwsAlreadyExistsError() async {
+//        var thrownError: Error?
+//        do {
+//            let _ = try await PassageAuth.register(identifier: registeredUserEmail)
+//        } catch {
+//            thrownError = error
+//        }
+//        XCTAssertEqual(thrownError as? PassageError, .userAlreadyExists)
+//    }
+//
+//    func testPassageAuth_whenLoginExistingUser_returnsValidAuthResult() async {
+//        let result = try? await PassageAuth.login(identifier: registeredUserEmail)
+//        if #available(iOS 16.0, *) {
+//            XCTAssertTrue(result?.authResult is AuthResult)
+//        } else {
+//            XCTAssertTrue(result?.magicLink is MagicLink)
+//        }
+//    }
+//
+//    func testPassageAuth_whenRegisterNewUser_returnsValidAuthResult() async {
+//        let result = try? await PassageAuth.register(identifier: unregisteredUserEmail)
+//        if #available(iOS 16.0, *) {
+//            XCTAssertTrue(result?.authResult is AuthResult)
+//        } else {
+//            XCTAssertTrue(result?.magicLink is MagicLink)
+//        }
+//    }
+//
+//    func testPassageAuth_whenGetExistingUser_returnsUser() async {
+//        let user = try? await PassageAuth.getUser(identifier: registeredUserEmail)
+//        XCTAssertNotNil(user)
+//    }
+//
+//    func testPassageAuth_whenGetNonexistingUser_throwsError() async {
+//        var thrownError: Error?
+//        do {
+//            let _ = try await PassageAuth.getUser(identifier: unregisteredUserEmail)
+//        } catch {
+//            thrownError = error
+//        }
+//        XCTAssertEqual(thrownError as? PassageError, .userDoesNotExist)
+//    }
+//
 
 }
